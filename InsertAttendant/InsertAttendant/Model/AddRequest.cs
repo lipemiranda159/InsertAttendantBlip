46136b4532fbf6d319945a633620447439f4a032
Felipe Alexandre Miranda (Take.net) <FelipeM@take.net>
2019-08-23 17:24:42 -0300

    create insert attendantBlip

---
+++ InsertAttendant/InsertAttendant/Model/AddRequest.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsertAttendant.Model
+{
+    [JsonObject("resource")]
+    public class Resource
+    {
+        public string identity { get; set; }
+        public List<string> teams { get; set; }
+    }
+
+    [JsonObject("resource")]
+    public class ResourceDelegation
+    {
+        public string target { get; set; }
+        public List<string> envelopeTypes { get; set; }
+    }
+
+    public class Metadata
+    {
+        [JsonProperty("blip_portal.email")]
+        public string portal_email { get; set; }
+        [JsonProperty("server.shouldStore")]
+        public string server_shouldStore { get; set; }
+    }
+
+    public class DelegationRequest
+    {
+        public string method { get; set; }
+        public string to { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+        public ResourceDelegation resource { get; set; }
+        public string id { get; set; }
+
+    }
+
+    public class AddRequest
+    {
+        public string method { get; set; }
+        public string to { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+        public Resource resource { get; set; }
+        public string id { get; set; }
+
+    }
+}
---
+++ InsertAttendant/InsertAttendant/Program.cs
@@ -0,0 +1,74 @@
+﻿using InsertAttendant.Model;
+using Lime.Protocol;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web;
+
+namespace InsertAttendant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a chave do bot");
+            var key = Console.ReadLine();
+
+            Console.WriteLine("Digite o caminho do arquivo com os usuários:");
+            var file = Console.ReadLine();
+
+            var text = System.IO.File.ReadAllLines(file);
+
+            foreach (var item in text)
+            {
+                var splitedText = item.Split(',');
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://msging.net");
+                    client.DefaultRequestHeaders.Add("Authorization",key);
+
+                    var request = new DelegationRequest()
+                    {
+                        id = EnvelopeId.NewId(),
+                        method = "set",
+                        resource = new ResourceDelegation()
+                        {
+                            envelopeTypes = new System.Collections.Generic.List<string>() { "command" },
+                            target = $"{HttpUtility.UrlEncode(splitedText[0])}@blip.ai"
+                        },
+                        to = "postmaster@msging.net",
+                        type = "application/vnd.lime.delegation+json",
+                        uri = "/delegations"
+                    };
+
+                    var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.Default, "application/json");
+                    var responsePost = client.PostAsync("/commands", content).Result;
+
+                    var requestAdd = new AddRequest()
+                    {
+                        id = EnvelopeId.NewId(),
+                        method = "set",
+                        resource = new Resource()
+                        {
+                            identity = $"{HttpUtility.UrlEncode(splitedText[0])}@blip.ai",
+                            teams = splitedText[1].Split(';').ToList()
+                        },
+                        to = "postmaster@desk.msging.net",
+                        type = "application/vnd.iris.desk.attendant+json",
+                        uri = "/attendants"
+                    };
+
+                    content = new StringContent(JsonConvert.SerializeObject(requestAdd), Encoding.Default, "application/json");
+                    responsePost = client.PostAsync("/commands", content).Result;
+
+                }
+            }
+
+
+
+        }
+    }
+}
